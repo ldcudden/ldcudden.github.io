bce11e651ae14485cbb7327f21dcafb71e9d9d8f
ldcudden <ldcudden@sbcglobal.net>
2015-07-25 15:01:24 -0700

    testing

--- m2/browse.php
+++ m2/browse.php
@@ -15,7 +15,7 @@ foreach ($items as $item => $os) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
     <title>Business Site</title>
     <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,900' rel='stylesheet' type='text/css'>
